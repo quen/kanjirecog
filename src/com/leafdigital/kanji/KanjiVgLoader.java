70d9d6a8cbd8a705ab2c3cb8b3ca233dbf89de78
sam marshall <sam@leafdigital.com>
2011-04-10 11:18:46 +0100

    Support for splitting database into small chunks
    Android needs this because there's a 1MB limit for compressed assets (sigh).

--- src/com/leafdigital/kanji/KanjiVgLoader.java
+++ src/com/leafdigital/kanji/KanjiVgLoader.java
@@ -26,10 +26,10 @@ import javax.xml.parsers.*;
 import org.xml.sax.*;
 import org.xml.sax.helpers.DefaultHandler;
 
-/** 
+/**
  * Reads kanji stroke order data from a file in KanjiVG format and simplifies
  * it into the basic stroke data used for recognition.
- * http://kanjivg.tagaini.net/ 
+ * http://kanjivg.tagaini.net/
  */
 public class KanjiVgLoader
 {
@@ -37,14 +37,14 @@ public class KanjiVgLoader
 	private LinkedList<KanjiInfo> read = new LinkedList<KanjiInfo>();
 	private LinkedList<String> warnings = new LinkedList<String>();
 	private HashSet<Integer> done = new HashSet<Integer>();
-	
+
 	/**
 	 * SAX handler.
 	 */
 	private class Handler extends DefaultHandler
 	{
 		private KanjiInfo current = null;
-		
+
 		@Override
 		public void startElement(String uri, String localName, String qName,
 			Attributes attributes) throws SAXException
@@ -53,7 +53,7 @@ public class KanjiVgLoader
 			{
 				// Clear current just in case
 				current = null;
-				
+
 				// Note: I used the midashi attribute initially, but had problems
 				// with the parser bizarrely misinterpreting some four-byte sequences.
 				String id = attributes.getValue("id");
@@ -77,13 +77,13 @@ public class KanjiVgLoader
 					warnings.add("<kanji> duplicate id= (" + id + ")");
 					return;
 				}
-				
+
 				// Check if code point is actually a CJK ideograph
 				String kanjiString = new String(Character.toChars(codePoint));
-				if((codePoint >= 0x4e00 && codePoint <= 0x9fff) 
-					|| (codePoint >= 0x3400 && codePoint <= 0x4dff) 
-					|| (codePoint >= 0x20000 && codePoint <= 0x2a6df) 
-					|| (codePoint >= 0xf900 && codePoint <= 0xfaff) 
+				if((codePoint >= 0x4e00 && codePoint <= 0x9fff)
+					|| (codePoint >= 0x3400 && codePoint <= 0x4dff)
+					|| (codePoint >= 0x20000 && codePoint <= 0x2a6df)
+					|| (codePoint >= 0xf900 && codePoint <= 0xfaff)
 					|| (codePoint >= 0x2f800 && codePoint <= 0x2fa1f))
 				{
 					current = new KanjiInfo(kanjiString);
@@ -121,7 +121,7 @@ public class KanjiVgLoader
 				}
 			}
 		}
-		
+
 		@Override
 		public void endElement(String uri, String localName, String qName)
 			throws SAXException
@@ -136,7 +136,7 @@ public class KanjiVgLoader
 			}
 		}
 	}
-	
+
 	/**
 	 * Constructs ready to read data.
 	 * @param input Input stream (will be closed after {@link #loadKanji()}
@@ -146,7 +146,7 @@ public class KanjiVgLoader
 	{
 		this.input = input;
 	}
-	
+
 	/**
 	 * Loads all kanji from the file and closes it.
 	 * @return All kanji as array
@@ -158,9 +158,9 @@ public class KanjiVgLoader
 		{
 			throw new IOException("Cannot load kanji more than once");
 		}
-		
+
 		// Parse data
-		SAXParser parser;		
+		SAXParser parser;
 		try
 		{
 			parser = SAXParserFactory.newInstance().newSAXParser();
@@ -179,7 +179,7 @@ public class KanjiVgLoader
 			x.initCause(e);
 			throw x;
 		}
-		
+
 		// Return result
 		return read.toArray(new KanjiInfo[read.size()]);
 	}
@@ -191,34 +191,54 @@ public class KanjiVgLoader
 	{
 		return warnings.toArray(new String[warnings.size()]);
 	}
-	
+
 	/**
 	 * Convert KanjiVG file into new info file.
 	 * @param args Filename to convert and output filename
 	 */
 	public static void main(String[] args)
 	{
-		if(args.length != 2)
+		if(args.length < 2 || args.length > 3
+			|| (args.length==3 && !args[2].matches("[0-9]{1,5}")))
 		{
 			System.err.println("Incorrect command line arguments. Syntax:\n"
-				+ "KanjiVgLoader <kanjivgfile> <output file>");
+				+ "KanjiVgLoader <kanjivgfile> <output file> [max size in kb]\n"
+				+ "Max size is used to optionally split the file into multiple\n"
+				+ "parts.");
 			return;
 		}
-		
+
 		File in = new File(args[0]);
 		if(!in.canRead())
 		{
 			System.err.println("Unable to read input file: " + args[0]);
 			return;
 		}
-		
-		File out = new File(args[1]);
-		if(out.exists())
+
+		File out;
+		int maxBytes = -1;
+		String fileName = args[1];
+		if(args.length == 3)
 		{
-			System.err.println("Output file already exists: " + args[1]);
-			return;
+			maxBytes = Integer.parseInt(args[2]) * 1024;
+
+			out = new File(fileName + ".1");
+			if(out.exists())
+			{
+				System.err.println("Output file already exists: " + fileName + ".1");
+				return;
+			}
 		}
-		
+		else
+		{
+			out = new File(fileName);
+			if(out.exists())
+			{
+				System.err.println("Output file already exists: " + fileName);
+				return;
+			}
+		}
+
 		try
 		{
 			// Load everything
@@ -237,15 +257,26 @@ public class KanjiVgLoader
 				}
 				System.out.println();
 			}
-			
+
 			KanjiList list = new KanjiList();
 			for(KanjiInfo kanji : allKanji)
 			{
 				list.add(kanji);
 			}
-			
-			System.out.println("Writing output file: " + out.getName());
-			list.save(new FileOutputStream(out), in.getName());
+
+			OutputStream stream;
+			if(maxBytes == -1)
+			{
+				System.out.println("Writing output file: " + out.getName());
+				stream = new FileOutputStream(out);
+			}
+			else
+			{
+				System.out.println("Writing output files: " + fileName + ".*");
+				stream = new SplitOutputStream(fileName, maxBytes);
+			}
+			list.save(stream, in.getName());
+			stream.close();
 		}
 		catch(IOException e)
 		{
@@ -254,6 +285,91 @@ public class KanjiVgLoader
 			System.err.println("FULL STACK TRACE:");
 			System.err.println();
 			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Output stream capable of writing multiple files; the first has .1, the
+	 * second .2, etc.
+	 */
+	private static class SplitOutputStream extends OutputStream
+	{
+		private String basePath;
+		private int maxBytes;
+
+		private OutputStream out;
+		private int bytesLeft;
+		private int index;
+
+		private SplitOutputStream(String basePath, int maxBytes) throws IOException
+		{
+			this.basePath = basePath;
+			this.maxBytes = maxBytes;
+			index = 0;
+		}
+
+		@Override
+		public void close() throws IOException
+		{
+			if(out != null)
+			{
+				out.close();
+				out = null;
+			}
+		}
+
+		private void checkOutput() throws IOException
+		{
+			if(out == null)
+			{
+				index++;
+				File file = new File(basePath + "." + index);
+				if(file.exists())
+				{
+					throw new IOException("File already exists: " + file.getPath());
+				}
+				out = new FileOutputStream(file);
+				bytesLeft = maxBytes;
+			}
+		}
+
+		private void wroteBytes(int wrote) throws IOException
+		{
+			bytesLeft -= wrote;
+			if(bytesLeft <= 0)
+			{
+				out.close();
+				out = null;
+			}
+		}
+
+		@Override
+		public void write(int oneByte) throws IOException
+		{
+			checkOutput();
+			out.write(oneByte);
+			wroteBytes(1);
+		}
+
+		@Override
+		public void write(byte[] buffer, int offset, int count) throws IOException
+		{
+			checkOutput();
+			if(count > bytesLeft)
+			{
+				int wasLeft = bytesLeft;
+				write(buffer, offset, bytesLeft);
+				write(buffer, offset + wasLeft, count - wasLeft);
+				return;
+			}
+			out.write(buffer, offset, count);
+			wroteBytes(count);
+		}
+
+		@Override
+		public void write(byte[] buffer) throws IOException
+		{
+			write(buffer, 0, buffer.length);
 		}
 	}
 }
